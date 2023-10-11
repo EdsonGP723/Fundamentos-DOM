5693323b109a91b4b29e7b693ceb3e4b676dbbdf
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-10-11 09:25:41 -0600

    Terminando práctica

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Fundamentos-DOM
  
+## Edson Gutierrez Prado
+
+### Parcial 3
+
+[Práctica 1](dom.js)
+
+[Página]()
---
+++ dom.js
@@ -0,0 +1,29 @@
+let $h1 = document.querySelector("h1");
+$h1.innerHTML = "<h1>Edson Gutierrez Prado</h1>"
+$h1.style.fontFamily = "Courier New";
+$h1.style.backgroundColor = "#000000";
+$h1.style.color = "#F0FFF0";
+
+let $img = document.querySelector("img");
+$img.src = "Edson.jpeg"
+$img.style.borderRadius= "50%";
+$img.style.maxWidth = "15%";
+$img.style.height = "auto";
+
+let $p = document.querySelector("p");
+$p.innerText = "Experiencia con Unity 2D y 3D, así como experiencia haciendo rigs en blender y animación 3d con blender"
+$p.style.fontFamily = "Courier New";
+$p.style.backgroundColor = "#000000";
+$p.style.color = "#F0FFF0";
+
+let $ol = document.querySelector("ol");
+$ol.innerHTML = "<li>Ver Películas</li><li>Jugar Videojuegos</li><li>Salir con mis amigos</li><li>Jugar D&D con mi familia y amigos</li>";
+$ol.style.fontFamily = "Courier New";
+$ol.style.backgroundColor = "#000000";
+$ol.style.color = "#F0FFF0";
+
+let $ul = document.querySelector("ul");
+$ul.innerHTML = '<li>edsongp107@outlook.com</li><li>55 47796350</li>';
+$ul.style.fontFamily = "Courier New";
+$ul.style.backgroundColor = "#000000";
+$ul.style.color = "#F0FFF0";
